227b9babc29ef60011f77ff17a5edb9c3b2c785c
venator721 <jpcamposcamargo@gmail.com>
2024-06-06 14:16:45 -0300

    Add files via upload

---
+++ olaMundo.py
@@ -0,0 +1,7 @@
+nome = "Joao Pedro Campos Camargo"
+idade = 16
+cidade = "Mogi das Cruzes -SP"
+formacao = "estudando python"
+
+print(f"Ola meu nome e {nome} e tenho {idade} anos de idade.")
+print(f"Moro em {cidade} e atualmente estou {formacao}.")
